d3cb7749c5e753300cc504c5bdd15cb4df22b9ae
Gabriel Afonso <afonso@divux.com>
2015-03-06 17:54:23 -0430

    Initial Commit

---
+++ ImaginamosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImaginamosTest
+//
+//  Created by Gabriel Afonso on 3/6/15.
+//  Copyright (c) 2015 Gabriel Afonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImaginamosTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImaginamosTest
+//
+//  Created by Gabriel Afonso on 3/6/15.
+//  Copyright (c) 2015 Gabriel Afonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
